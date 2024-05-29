2f4641f8c78aefa174fff51e45329b23381750b0
leon.kepler <leon.kepler@bytedance.com>
2024-05-29 18:03:54 +0800

    new

--- utils/merge_lora.py
+++ utils/merge_lora.py
@@ -7,23 +7,24 @@ from peft import PeftModel
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-def merge_lora(base_model_name, lora_path):
+def merge_lora(base_model_name, lora_path, target_path):
 
     base_model = AutoModelForCausalLM.from_pretrained(base_model_name)
     peft_model = PeftModel.from_pretrained(base_model, lora_path)
     tokenizer = AutoTokenizer.from_pretrained(base_model_name, use_fast=False)
 
     model = peft_model.merge_and_unload()
-    target_model_path = lora_path.replace("checkpoint", "full")
-    model.save_pretrained(target_model_path)
-    tokenizer.save_pretrained(target_model_path)
+    # target_model_path = lora_path.replace("checkpoint", "full")
+    model.save_pretrained(target_path)
+    tokenizer.save_pretrained(target_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--base_model_path", type=str, default=None)
     parser.add_argument("--lora_path", type=str, required=True)
+    parser.add_argument("--target_path", type=str, required=True)
 
     args = parser.parse_args()
 
-    merge_lora(args.base_model_path, args.lora_path)
+    merge_lora(args.base_model_path, args.lora_path, args.target_path)
